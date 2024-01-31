52129a0e842e4143bea72bfcdee6f8f8639c77c9
Hamad Ali <70281291+Deucalio@users.noreply.github.com>
2024-01-31 15:48:11 +0500

    Update Main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -29,7 +29,7 @@ const Main = () => {
             href="https://drive.google.com/file/d/1iFli-h_skT1EB1TP8FCeTCqiVXtgokfZ/view?usp=sharing"
             className="my-1 w-fit border-2 border-[#DBD7D2] px-8 py-2 uppercase transition-all duration-1000 hover:border-black hover:bg-white hover:text-black"
           >
-            Resume
+            CV
           </a>
         </article>
       </main>
